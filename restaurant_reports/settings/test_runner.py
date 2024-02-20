50d2d1641779e38eacb3615382231ee941a2ede5
aakash98 <imaakash98@gmail.com>
2024-02-21 03:47:32 +0530

    Adding Files For Syncing

--- restaurant_reports/settings/test_runner.py
+++ restaurant_reports/settings/test_runner.py
@@ -1,17 +1,17 @@
-import sys
-from django.conf import settings
-from django.test.runner import DiscoverRunner
-from django.db import connection
-
-class CustomTestRunner(DiscoverRunner):
-    def setup_test_environment(self, *args, **kwargs):
-        super().setup_test_environment(*args, **kwargs)
-        settings.DATABASES['default']['NAME'] = 'test_database'  # Replace 'test_database' with the name of your MySQL test database
-
-    def teardown_test_environment(self, *args, **kwargs):
-        super().teardown_test_environment(*args, **kwargs)
-        connection.close()
-
-if __name__ == "__main__":
-    pass
-    # sys.exit(CustomTestRunner().run_tests(sys.argv[1:]))
+# import sys
+# from django.conf import settings
+# from django.test.runner import DiscoverRunner
+# from django.db import connection
+#
+# class CustomTestRunner(DiscoverRunner):
+#     def setup_test_environment(self, *args, **kwargs):
+#         super().setup_test_environment(*args, **kwargs)
+#         settings.DATABASES['default']['NAME'] = 'test_database'  # Replace 'test_database' with the name of your MySQL test database
+#
+#     def teardown_test_environment(self, *args, **kwargs):
+#         super().teardown_test_environment(*args, **kwargs)
+#         connection.close()
+#
+# if __name__ == "__main__":
+#     pass
+#     # sys.exit(CustomTestRunner().run_tests(sys.argv[1:]))
